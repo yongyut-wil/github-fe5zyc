1b22f5f4f395e1181a5a79c36be2faf5be729f6f
yongyut-wil <yongyut.wilachan@gmail.com>
2022-04-17 18:16:03 +0700

    2 files modified

--- src/routes/invoice.jsx
+++ src/routes/invoice.jsx
@@ -1,8 +1,20 @@
 import { useParams } from 'react-router-dom';
-import { getInvoice } from "../data";
+import { getInvoice } from '../data';
 
 export default function Invoice() {
   let params = useParams();
   let invoice = getInvoice(parseInt(params.invoiceId, 10));
-  return <h2>Invoice: {params.invoiceId}</h2>;
+  return (
+    <div>
+      
+      <main style={{ padding: "1rem" }}>
+      <h2>Invoice: {params.invoiceId}</h2>
+      <h2>Total Due: {invoice.amount}</h2>
+      <p>
+        {invoice.name}: {invoice.number}
+      </p>
+      <p>Due Date: {invoice.due}</p>
+    </main>
+    </div>
+  );
 }

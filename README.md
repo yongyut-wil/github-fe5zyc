314de358780cdd7cffa201d98f7a7ea9f1a3eae4
yongyut-wil <yongyut.wilachan@gmail.com>
2022-04-17 16:28:50 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-fe5zyc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-fe5zyc)
---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import App from "./App";
+import "./main.css";
+
+let rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
